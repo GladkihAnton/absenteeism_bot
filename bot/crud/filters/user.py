371dd4cfe91ffc3b1e653bdede3f4552bd0af57b
alexeymyakinin <alexeymyakinin@gmail.com>
2022-05-28 18:52:31 +0300

    Add cmd

--- bot/crud/filters/user.py
+++ bot/crud/filters/user.py
@@ -7,7 +7,7 @@ from sqlalchemy.sql.elements import BinaryExpression, or_
 
 def get_user_filters(**kwargs) -> List[BinaryExpression]:
     filters = []
-    if user_id := kwargs.get("user_id"):
+    if user_id := kwargs.get("telegram_user_id"):
         filters.append(User.telegram_user_id == user_id)
 
     if role := kwargs.get("role"):
