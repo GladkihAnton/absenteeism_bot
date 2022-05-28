88a01842456f1f5c340b55d93c2900782f3e8190
Антон Гладких <AAGladkikh1@sberbank.ru>
2022-05-28 15:13:26 +0300

    Deleted chat id

--- scripts/postgres/schema.sql
+++ scripts/postgres/schema.sql
@@ -18,13 +18,11 @@ CREATE TABLE absence.role
 CREATE TABLE absence.user
 (
     telegram_user_id int primary key,
-    telegram_chat_id int unique not null,
     name             varchar,
     role_id          int references absence.role (id),
     office_id        int references absence.office (id)
 );
 CREATE INDEX user_telegram_user_id_ix ON absence.user (telegram_user_id);
-CREATE INDEX user_telegram_chat_id_ix ON absence.user (telegram_chat_id);
 
 
 CREATE TABLE absence.absence
