55cad8dce536cee2c2320e7dcba9680f8c5496b4
Антон Гладких <AAGladkikh1@sberbank.ru>
2022-05-28 18:49:16 +0300

    Admin panel init

---
+++ admin_panel/absence/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+class TelegramUser(models.Model):
+    telegram_user_id = models.IntegerField(primary_key=True, null=False)
+    name = models.CharField(max_length=256, verbose_name='Имя в телеграме')
+    role = models.ForeignKey('Role', on_delete=models.SET_NULL, null=True, blank=False)
+    office = models.ForeignKey('Office', on_delete=models.SET_NULL, null=True, blank=False)
+
+    active = models.BooleanField(default=False, null=False, blank=False)
+
+    class Meta:
+        db_table = '"absence"."user"'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=256, null=False, blank=False)
+
+    class Meta:
+        db_table = '"absence"."role"'
+        verbose_name = 'Роль'
+        verbose_name_plural = 'Роли'
+
+
+class Office(models.Model):
+    name = models.CharField(max_length=256, null=False, blank=False)
+
+    class Meta:
+        db_table = '"absence"."office"'
+        verbose_name = 'Роль'
+        verbose_name_plural = 'Роли'
+
+
+class Absence(models.Model):
+    date = models.DateField(null=False)
+    message = models.CharField(max_length=256, null=False, blank=False)
+
+    telegram_user = models.ForeignKey(TelegramUser, on_delete=models.PROTECT, db_index=True)
+
+    class Meta:
+        db_table = '"absence"."absence"'
+        verbose_name = 'Пропуск'
+        verbose_name_plural = 'Пропуски'
---
+++ admin_panel/admin_panel/settings.py
@@ -0,0 +1,93 @@
+import logging
+from pathlib import Path
+
+from .env import env
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = env('SECRET_KEY')
+
+DEBUG = env('DEBUG')
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+CSRF_TRUSTED_ORIGINS = [f'http://{host}' for host in env.list('ALLOWED_HOSTS')] + [
+    f'https://{host}' for host in env.list('ALLOWED_HOSTS')
+]
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'absence',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'admin_panel.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'template'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'admin_panel.wsgi.application'
+
+
+DATABASES = {
+    'default': env.db('DATABASE_URL'),
+}
+
+logging.error(DATABASES)
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
